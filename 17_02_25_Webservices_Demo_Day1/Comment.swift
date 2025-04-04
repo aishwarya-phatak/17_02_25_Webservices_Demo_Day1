b140ea080f6084ea730d98a8812a12a2584a7a1b
aishwarya <ashp17star@gmail.com>
2025-04-04 15:44:33 +0530

    .

--- 17_02_25_Webservices_Demo_Day1/Comment.swift
+++ 17_02_25_Webservices_Demo_Day1/Comment.swift
@@ -5,11 +5,8 @@
 //  Created by Vishal Jagtap on 03/04/25.
 //
 
-struct APIResponse{
-    var comments : [Comment]
-}
 
-struct Comment{
+struct Comment : Decodable{
     var postId : Int
     var id : Int
     var name : String

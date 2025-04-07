7d8aa97111c5552f04c4eec107f0b3c2090fd850
aishwarya <ashp17star@gmail.com>
2025-04-07 15:56:34 +0530

    Binding data with TableView Cell

---
+++ 17_02_25_Webservices_Demo_Day1/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  17_02_25_Webservices_Demo_Day1
+//
+//  Created by Vishal Jagtap on 07/04/25.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
